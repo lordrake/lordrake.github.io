1f25c26d6a6077530cfb82432d9236781a9f3c5c
Danilo Colella <danilo.colella@deptagency.com>
2024-11-23 18:14:58 +0100

    add header component and update page title

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import { FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { IoIosSend } from 'react-icons/io'
+
+function Header() {
+  return (
+    <nav className='flex items-center justify-between py-6'>
+      <div className='flex flex-shrink-0 items-center'>
+        <a href='/' aria-label='Home' className='mx-2 text-4xl font-light'>
+          Danilo Colella
+        </a>
+      </div>
+
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <a
+          href='https://www.linkedin.com/in/danilo-colella-5b545610a/'
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='LinkedIn'
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href='https://www.instagram.com/danilo.colella/'
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='Instagram'
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href='mailto:danilocolella17@gmail.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          aria-label='LinkedIn'
+        >
+          <IoIosSend />
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
