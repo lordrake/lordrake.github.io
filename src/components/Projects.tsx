afbe61b8e25725d646605df15e402c46679d1899
Danilo Colella <danilo.colella@deptagency.com>
2024-11-26 13:44:21 +0100

    fix projects alignment on dekstop and mobile

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,22 +5,22 @@ function Projects() {
   return (
     <section className='pb-4'>
       <h2 className='my-20 text-center text-4xl'>Projects</h2>
-      <div>
+      <div className='block md:grid grid-cols-2 gap-16 max-w-3xl mx-auto'>
         {PROJECTS.map((project, index) => (
           <div
             key={index}
-            className='mb-12 flex flex-col lg:flex-row items-center lg:items-start flex-wrap lg:justify-center gap-x-6 gap-y-6'
+            className='mb-12 flex flex-col lg:flex-row items-center justify-center lg:items-start flex-wrap gap-x-6 gap-y-6'
           >
-            <div className='w-auto flex-shrink lg:w-1/4'>
+            <div className='w-full lg:w-1/2 flex-shrink'>
               <img
                 src={project.image}
                 alt={project.title}
-                width={250}
+                width={'100%'}
                 height={250}
                 className='rounded'
               />
             </div>
-            <div className='w-auto max-w-xl lg:w-3/4'>
+            <div className='w-full max-w-xl lg:w-3/4 lg:text-center'>
               <h3 className='mb-2 font-semibold text-2xl'>{project.title}</h3>
               <p className='mb-4 text-stone-400'>{project.description}</p>
               {project.technologies.map((tech, index) => (
