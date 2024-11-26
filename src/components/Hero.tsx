4910c1a1ca19f26c22f42cc8ab64aaedf708ff56
Danilo Colella <danilo.colella@deptagency.com>
2024-11-26 17:43:36 +0100

    add animation to hero and technologies

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,70 @@
 import profilePic from '../assets/danilo-colella-profile.png'
+import { motion } from 'framer-motion'
+
+const containerVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+const childVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.5 },
+  },
+}
 
 function Hero() {
   return (
     <div className='mx-auto w-fit pb-4 mt-8 lg:mb-36 lg:mt-16'>
-      <div className='flex flex-col-reverse  lg:flex-row-reverse gap-y-4 gap-x-16 items-center lg:justify-end'>
+      <motion.div
+        className='flex flex-col-reverse  lg:flex-row-reverse gap-y-4 gap-x-16 items-center lg:justify-end'
+        initial='hidden'
+        animate='visible'
+        variants={containerVariants}
+      >
         <div className='flex flex-col justify-center items-center lg:items-start'>
-          <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>
+          <motion.h2
+            className='pb-2 text-4xl tracking-tighter lg:text-8xl'
+            variants={childVariants}
+          >
             Danilo Colella
-          </h2>
-          <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>
+          </motion.h2>
+          <motion.span
+            className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'
+            variants={childVariants}
+          >
             Frontend Developer
-          </span>
+          </motion.span>
         </div>
-        <img
+        <motion.img
           src={profilePic}
           width={150}
           height={150}
           alt='Danilo Colella'
           className='border border-stone-900 rounded-3xl'
+          variants={childVariants}
         />
-      </div>
+      </motion.div>
 
-      <div className='flex flex-col items-center lg:items-start'>
-        <p className='my-2 max-w-3xl py-6 text-xl leading-relaxed tracking-tighter'>
+      <motion.div
+        className='flex flex-col items-center lg:items-start'
+        initial='hidden'
+        animate='visible'
+        variants={containerVariants}
+      >
+        <motion.p
+          className='my-2 max-w-3xl py-6 text-xl leading-relaxed tracking-tighter'
+          variants={childVariants}
+        >
           I'm a passionate front-end developer with 5 years of hands-on
           experience. I have honed my skills on front-end technologies such as
           React, Angular and Next.js, as well as backend technologies like
@@ -31,17 +73,18 @@ function Hero() {
           Bridging the gap between Development and Design, between Analytical
           and Creative approach, I like delivering effective products with
           exceptional user experiences.
-        </p>
-        <a
+        </motion.p>
+        <motion.a
           href='/danilo-colella-resume.pdf'
           target='_blank'
           rel='noopener noreferrer'
           download
           className='bg-white rounded-full p-4 text-sm text-stone-800'
+          variants={childVariants}
         >
           Download Resume
-        </a>
-      </div>
+        </motion.a>
+      </motion.div>
     </div>
   )
 }
--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -2,31 +2,87 @@ import { FaAngular, FaNodeJs } from 'react-icons/fa'
 import { RiReactjsLine } from 'react-icons/ri'
 import { SiMysql, SiTypescript } from 'react-icons/si'
 import { TbBrandNextjs } from 'react-icons/tb'
+import { motion } from 'framer-motion'
+
+const iconVariants = (duration: number) => ({
+  initial: { y: 10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: 'linear',
+      repeat: Infinity,
+      repeatType: 'reverse',
+    },
+  },
+})
 
 function Technologies() {
   return (
     <section className='pb-24'>
-      <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-      <div className='flex flex-wrap items-center justify-center gap-4'>
-        <div>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className='my-20 text-center text-4xl'
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        className='flex flex-wrap items-center justify-center gap-4'
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+      >
+        <motion.div
+          initial='initial'
+          animate='animate'
+          variants={iconVariants(2.5)}
+        >
           <RiReactjsLine className='text-7xl text-cyan-400' />
-        </div>
-        <div className='p-4'>
+        </motion.div>
+        <motion.div
+          className='p-4'
+          initial='initial'
+          animate='animate'
+          variants={iconVariants(3)}
+        >
           <TbBrandNextjs className='text-7xl' />
-        </div>
-        <div className='p-4'>
+        </motion.div>
+        <motion.div
+          className='p-4'
+          className='p-4'
+          initial='initial'
+          animate='animate'
+          variants={iconVariants(5)}
+        >
           <FaAngular className='text-7xl text-red-500' />
-        </div>
-        <div className='p-4'>
+        </motion.div>
+        <motion.div
+          className='p-4'
+          initial='initial'
+          animate='animate'
+          variants={iconVariants(2)}
+        >
           <SiTypescript className='text-6xl text-blue-400' />
-        </div>
-        <div className='p-4'>
+        </motion.div>
+        <motion.div
+          className='p-4'
+          initial='initial'
+          animate='animate'
+          variants={iconVariants(6)}
+        >
           <FaNodeJs className='text-6xl text-green-500' />
-        </div>
-        <div className='p-4'>
+        </motion.div>
+        <motion.div
+          className='p-4'
+          initial='initial'
+          animate='animate'
+          variants={iconVariants(4)}
+        >
           <SiMysql className='text-6xl text-orange-400' />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   )
 }
