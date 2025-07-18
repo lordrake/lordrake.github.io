508bbf4e2841ad8c992c47b14bf590c85ed3856a
danilo-colella <danilo.colella@deptagency.com>
2025-07-18 15:40:55 +0200

    add exclamation mark

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ import { CONTACT } from '../CONTENT'
 function Contact() {
   return (
     <div className='border-t border-stone-900 pb-20'>
-      <h2 className='my-10 text-center text-4xl'>Get in Touch</h2>
+      <h2 className='my-10 text-center text-4xl'>Get in Touch!</h2>
       <p className='my-4 text-center'>{CONTACT.address}</p>
       <div className='text-center tracking-tighter'>
         <a href={`mailto:${CONTACT.email}`} className='border-b'>
